f37d3ef3f49234e416626e4674babbed33bae99b
1521350289@qq.com <1521350289@qq.com>
2019-01-18 21:54:23 +0800

    slice

---
+++ slice/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear();
+    println!("{}", i);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
