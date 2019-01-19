272e18604443f12138446b35c2ee918b02ec14d5
1521350289@qq.com <1521350289@qq.com>
2019-01-19 23:09:33 +0800

    .

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,18 +1,21 @@
 fn main() {
     let mut s = String::from("hello world");
-    let word = first_word(&s);
+    let word = first_word(&mut s[..]);
+
+    let mut s1 = "world hello";
+    let word1 = first_word(&mut s1);
     s.clear();
-    println!("{}", word);
+    println!("{},{}", word, word1);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &mut str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
