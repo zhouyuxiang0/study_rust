9367a7c7c24efee822f2ecdea498fa35aaa5b6c7
1521350289@qq.com <1521350289@qq.com>
2019-06-07 22:00:52 +0800

    .

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,21 +1,83 @@
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
-use std::fs;
+use std::fs::File;
+
+struct ThreadPool;
+impl ThreadPool {
+    /// 创建线程池
+    /// 
+    /// 线程池中线程的数量
+    /// 
+    /// # Panics
+    /// 
+    /// `new`函数在size为0时会panic。
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+        ThreadPool{
+            workers
+        }
+    }
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static
+    {
+
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread,
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4)
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n", "hello.html");
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
     let contents = fs::read_to_string("hello.html").unwrap();
     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     stream.write(response.as_bytes()).unwrap();
